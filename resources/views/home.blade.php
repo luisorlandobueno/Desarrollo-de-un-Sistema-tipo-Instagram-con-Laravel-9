321c9c41301b095b2fb14b4bb9b9c4930b3b6418
luis orlando muñoz bueno <105979241+luisorlandobueno@users.noreply.github.com>
2023-11-30 08:07:42 -0500

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+
+@section('titulo')
+
+pagina principal
+
+@endsection
+
+
+@section('contenido')
+
+esto es el contenido
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        @vite('resources/css/app.css')
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+      
+
+     
+    </head>
+  <body class="bg-gray-100">
+
+    <h1 class="text-3xl font-bold underline"> clon de instagram</h1>
+    
+  </body>
+</html>
