3b41b05b84668f41df638739f5cf460e0a046031
luis orlando muñoz bueno <105979241+luisorlandobueno@users.noreply.github.com>
2023-11-30 08:09:11 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    "./resources/**/*.blade.php",
+    "./resources/**/*.blade.js",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+
+
+
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
